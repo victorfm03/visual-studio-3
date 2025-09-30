a3b2e1ec2da02a125b42c7c799800ab4bc0795b0
victorfm03 <xvfremor828@ieshnosmachado.org>
2025-09-30 10:28:20 +0200

    Auto save changes

---
+++ python/ej6.py
@@ -0,0 +1,25 @@
+num1= int(input("Numero 1: "))
+num2= int(input("Numero 2: "))
+num3= int(input("Numero 3: "))
+
+mayor=0
+menor=0
+
+if num1>num2 and num1>num3:
+    mayor=num1
+
+    if num2<num3:
+        menor=num2
+    else:
+    menor=num3
+
+elif num2>num3:
+    mayor=num2
+
+    if num1<num3:
+        menor=num1
+    else:
+        menor=num3
+
+else:
+mayor=num3
