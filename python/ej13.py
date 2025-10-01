c014b4abf06e348ea41fb7f93641bab8f729d130
victorfm03 <xvfremor828@ieshnosmachado.org>
2025-10-01 13:07:46 +0200

    Incluimos www para php

--- python/ej13.py
+++
@@ -1,4 +0,0 @@
-tamanyo=int(input("Di el tamaño: "))
-
-for a in range(tamanyo):
-    print("*"*a)
--- python/ej24.py
+++
@@ -1,7 +0,0 @@
-num=int(input("Di un numero: "))
-
-for a in range(num):
-
-    for b in range(num):
-        print(f"{(a+1)+(b+1)}",end=" ")
-    print()
