883a1cf23aa72c860082edde3dccd4928f014bbc
victorfm03 <xvfremor828@ieshnosmachado.org>
2025-10-01 12:42:36 +0200

    Auto save changes

---
+++ php/ej_formulario.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+	<p>Hola <?php echo $_GET['txtNombre'] ?>, aunque tienes <?php echo $_GET['txtEdad'] ?> 
+    años, estás hecho un chaval.</p>
+</body>
+</html>
