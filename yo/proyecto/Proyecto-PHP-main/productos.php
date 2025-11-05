83d96b21a0ed9585455f7965809f6d2a1c394bc2
victorfm03 <xvfremor828@ieshnosmachado.org>
2025-11-05 19:20:43 +0100

    Auto save changes

--- yo/proyecto/Proyecto-PHP-main/productos.php
+++ yo/proyecto/Proyecto-PHP-main/productos.php
@@ -10,10 +10,14 @@ $precio = $_POST['precio'] ?? '';
 $id_categoria = $_POST['id_categoria'] ?? '';
 $en_stock = isset($_POST['en_stock']) ? 1 : 0;
 
+// --- Mostrar PRODUCTO ---
+
+$sql_listado="SELECT p.*,c.category_name FROM product p, category c WHERE p.id_category=c.id_category";
+
 // --- INSERTAR PRODUCTO ---
 if ($accion == 'insertar') {
     try {
-        $sql = "INSERT INTO productos (nombre_producto, precio, id_categoria, en_stock)
+        $sql = "INSERT INTO product (product_name, price, id_category, in_stock)
                 VALUES (:nombre_producto, :precio, :id_categoria, :en_stock)";
         $stmt = $conexion->prepare($sql);
         $stmt->execute([
@@ -28,20 +32,26 @@ if ($accion == 'insertar') {
     }
 }
 
+//--- MOSTRAR CATEGORIAS ---
+
+$sql_categorias="SELECT * FROM category";
+$categorias =$conexion->query($sql_categorias);
+
 // --- BUSCAR PRODUCTO POR NOMBRE ---
 $producto_buscado = null;
 if ($accion == 'buscar') {
     $buscar_nombre = $_POST['buscar_nombre'] ?? '';
-    $stmt = $conexion->prepare("SELECT * FROM productos WHERE nombre_producto LIKE ?");
+    $stmt = $conexion->prepare("SELECT p.*, c.category_name FROM product p, category c WHERE p.product_name LIKE ? and p.id_category=c.id_category");
     $stmt->execute(["%$buscar_nombre%"]);
-    $producto_buscado = $stmt->fetch(PDO::FETCH_ASSOC);
+    $producto_buscado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
     if (!$producto_buscado) $mensaje = "No se encontró ningún producto con ese nombre.";
 }
 
 // --- BORRAR PRODUCTO ---
 if ($accion == 'borrar' && $id_producto) {
     try {
-        $stmt = $conexion->prepare("DELETE FROM productos WHERE id_producto = ?");
+        $stmt = $conexion->prepare("DELETE FROM product WHERE id_product = ?");
         $stmt->execute([$id_producto]);
         $mensaje = "Producto eliminado correctamente.";
     } catch (PDOException $e) {
@@ -56,7 +66,9 @@ if ($accion == 'borrar' && $id_producto) {
 <head>
     <meta charset="UTF-8">
     <title>Gestión de Productos</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
 </head>
 
 <body class="bg-light">
@@ -67,32 +79,81 @@ if ($accion == 'borrar' && $id_producto) {
             <div class="alert alert-info"><?= $mensaje ?></div>
         <?php endif; ?>
 
+        <div class="card mb-4">
+            <div class="card-header bg-success text-white">Lista de Productos</div>
+            <div class="card-body">
+                <table class='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOMBRE</th>
+                            <th>PRECIO</th>
+                            <th>CATEGORIA</th>
+                            <th>EN STOCK</th>
+                            <th>F.REGISTRO</th>
+                            <th>ACCIÓN</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+
+                            $statement =$conexion->query($sql_listado);
+
+                            foreach($statement as $fila){
+                                $mensaje ="";
+                                $mensaje .= "<tr><td>" . $fila['id_product'] . "</td>";
+                                $mensaje .= "<td>" . $fila['product_name'] . "</td>";
+                                $mensaje .= "<td>" . $fila['price'] . " €</td>";
+                                $mensaje .= "<td>" . $fila['category_name'] . "</td>";
+                                $mensaje .= "<td>" . ($fila['in_stock'] ==0? 'No' : 'Si'). "</td>";
+                                $mensaje .= "<td>" . $fila['registration_date'] . "</td>";
+
+                                $mensaje .= "<td><form class='d-inline me-1' action='productos.php' method='post'>";
+                                $mensaje .= "<input type='hidden' name='producto' value='" . htmlspecialchars(json_encode($fila),ENT_QUOTES) . "' />";
+                                $mensaje .= "<input type='hidden' name='accion' value='editar' />";
+                                $mensaje .= "<button name='Editar' class='btn btn-primary'><i class='bi bi-pencil-square'></i></button></form>";
+
+                                $mensaje .= "<form class='d-inline' action='productos.php' method='post'>";
+                                $mensaje .= "<input type='hidden' name='id_producto' value='" . $fila['id_product']  . "' />";
+                                $mensaje .= "<input type='hidden' name='accion' value='borrar' />";
+                                $mensaje .= "<button name='borrar' class='btn btn-danger'><i class='bi bi-trash'></i></button></form>";
+
+                                $mensaje .= "</td></tr>";
+
+                                echo $mensaje;
+                            }
+
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
         <!-- FORMULARIO DE ALTA / MODIFICACIÓN -->
         <div class="card mb-4">
             <div class="card-header bg-primary text-white">Agregar / Modificar Producto</div>
             <div class="card-body">
                 <form method="post">
-                    <input type="hidden" name="id_producto" value="<?= $producto_buscado['id_producto'] ?? '' ?>">
+                    <input type="hidden" name="id_producto" value="<?= $producto_buscado['id_product'] ?? '' ?>">
 
                     <div class="row mb-3">
                         <div class="col-md-4">
                             <label class="form-label">Nombre</label>
                             <input type="text" class="form-control" name="nombre_producto"
-                                value="<?= $producto_buscado['nombre_producto'] ?? '' ?>" required>
+                                value="<?= $producto_buscado['product_name'] ?? '' ?>" required>
                         </div>
                         <div class="col-md-3">
                             <label class="form-label">Precio (€)</label>
                             <input type="number" step="0.01" class="form-control" name="precio"
-                                value="<?= $producto_buscado['precio'] ?? '' ?>" required>
+                                value="<?= $producto_buscado['price'] ?? '' ?>" required>
                         </div>
                         <div class="col-md-3">
                             <label class="form-label">Categoría</label>
                             <select class="form-select" name="id_categoria" required>
-                                <option value="">Seleccionar</option>
+                                <option value="" disabled="disabled">Seleccionar</option>
                                 <?php foreach ($categorias as $cat): ?>
-                                    <option value="<?= $cat['id_categoria'] ?>"
-                                        <?= (isset($producto_buscado['id_categoria']) && $producto_buscado['id_categoria'] == $cat['id_categoria']) ? 'selected' : '' ?>>
-                                        <?= $cat['nombre_categoria'] ?>
+                                    <option value="<?= $cat['id_category'] ?>"
+                                        <?= (isset($producto_buscado['id_category']) && $producto_buscado['id_category'] == $cat['id_category']) ? 'selected' : '' ?>>
+                                        <?= $cat['category_name'] ?>
                                     </option>
                                 <?php endforeach; ?>
                             </select>
@@ -100,7 +161,7 @@ if ($accion == 'borrar' && $id_producto) {
                         <div class="col-md-2 d-flex align-items-center">
                             <div class="form-check mt-3">
                                 <input class="form-check-input" type="checkbox" name="en_stock"
-                                    <?= (!empty($producto_buscado['en_stock'])) ? 'checked' : '' ?>>
+                                    <?= (!empty($producto_buscado['in_stock'])) ? 'checked' : '' ?>>
                                 <label class="form-check-label">En stock</label>
                             </div>
                         </div>
@@ -124,11 +185,46 @@ if ($accion == 'borrar' && $id_producto) {
                         <button type="submit" name="accion" value="buscar" class="btn btn-dark">Buscar</button>
                     </div>
                 </form>
+
+                <?php if($producto_buscado):?>
+                <table class='table table-striped'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOMBRE</th>
+                            <th>PRECIO</th>
+                            <th>CATEGORIA</th>
+                            <th>EN STOCK</th>
+                            <th>F.REGISTRO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+
+                            foreach($producto_buscado as $fila){
+                                $mensaje ="";
+                                $mensaje .= "<tr><td>" . $fila['id_product'] . "</td>";
+                                $mensaje .= "<td>" . $fila['product_name'] . "</td>";
+                                $mensaje .= "<td>" . $fila['price'] . " €</td>";
+                                $mensaje .= "<td>" . $fila['category_name'] . "</td>";
+                                $mensaje .= "<td>" . ($fila['in_stock'] ==0? 'No' : 'Si'). "</td>";
+                                $mensaje .= "<td>" . $fila['registration_date'] . "</td>";
+
+                                $mensaje .= "</td></tr>";
+
+                                echo $mensaje;
+                            }
+
+                        ?>
+                    </tbody>
+                </table>
+                <?php endif;?>
             </div>
         </div>
     </div>
 
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
 
 </html>
