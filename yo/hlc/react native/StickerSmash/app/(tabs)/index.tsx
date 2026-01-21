0699f1d5c0f2f8098d419d45b4d352505e39dd32
victorfm03 <xvfremor828@ieshnosmachado.org>
2026-01-21 14:59:39 +0100

    Auto save changes

---
+++ yo/hlc/react native/StickerSmash/app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import { View, StyleSheet } from 'react-native';
+
+import Button from '@/components/Button'; 
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
