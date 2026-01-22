b445219f067a33c74ef1ffbf03a34ab1ad1788f2
victorfm03 <xvfremor828@ieshnosmachado.org>
2026-01-22 11:29:22 +0100

    Auto save changes

--- yo/diw/React/FactsdeChuckNorris/src/main.jsx
+++ yo/diw/React/FactsdeChuckNorris/src/main.jsx
@@ -1,9 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+/**
+ * @fileoverview Punto de entrada de la aplicación React
+ * Este archivo configura e inicializa la aplicación React en el DOM
+ */
 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+/**
+ * Inicializa y renderiza la aplicación React
+ * 
+ * Proceso:
+ * 1. Obtiene el elemento root del DOM (usualmente un div con id="root")
+ * 2. Crea la raíz de React usando createRoot
+ * 3. Renderiza el componente App envuelto en StrictMode
+ * 
+ * StrictMode es un componente de desarrollo que:
+ * - Resalta posibles problemas en la aplicación
+ * - Ejecuta ciertos checks y warnings en desarrollo
+ * - No tiene impacto en la compilación final de producción
+ */
 createRoot(document.getElementById('root')).render(
+  // StrictMode: Componente que activa verificaciones adicionales en desarrollo
   <StrictMode>
+    {/* Componente principal de la aplicación */}
     <App />
   </StrictMode>,
 )
